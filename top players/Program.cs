9276809ff3f2af154f944de571d423b08f6a8979
Virvon <dan130205@gmail.com>
2022-11-25 12:01:20 +0300

    top players

---
+++ top players/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace top_players
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database(new List<Player>()
+            {
+                new Player("мегаНекит", 101, 27),
+                new Player("Crisp", 24, 5),
+                new Player("fufak", 103, 26),
+                new Player("Svin", 57, 19),
+                new Player("Миха", 80, 23),
+                new Player("Степа", 12, 2),
+                new Player("Газкул", 114, 29),
+            });
+            int count = 3;
+
+            database.SortByLevel();
+
+            Console.WriteLine($"Топ {count} по Уровню:");
+
+            database.ShowTop(count);
+            database.SortByStrength();
+
+            Console.WriteLine($"\nТоп {count} по Силе:");
+
+            database.ShowTop(count);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Database
+    {
+        private List<Player> _database;
+
+        public Database(List<Player> database)
+        {
+            _database = database;
+        }
+
+        public void SortByLevel()
+        {
+            _database = _database.OrderByDescending(player => player.Level).ToList();
+        }
+
+        public void SortByStrength()
+        {
+            _database = _database.OrderByDescending(player => player.Strength).ToList();
+        }
+
+        public void ShowTop(int count)
+        {
+            if(count > _database.Count)
+                count = _database.Count;
+
+            for(int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{_database[i].Name}");
+            }
+        }
+    }
+
+
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public int Strength { get; private set; }
+
+        public Player(string name, int level, int strength)
+        {
+            Name = name;
+            Level = level;
+            Strength = strength;
+        }
+    }
+}
---
+++ top players/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("top players")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("top players")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("233e3e21-7de5-4be7-ba87-0bb47258d302")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
